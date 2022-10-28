76675f43c835d3539ecdcb75dd631c8310604927
lagccl <30540361+lagccl@users.noreply.github.com>
2022-10-28 15:48:06 -0300

    Update test.java

--- src/main/java/org/soyphea/k8s/srevice/test.java
+++ src/main/java/org/soyphea/k8s/srevice/test.java
@@ -1,13 +1,4 @@
-public class test extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // The intent used to start this exported component is retrieved
-        Intent intent = getIntent();
-
-        // extract the embedded Intent
-        Intent forward = (Intent) intent.getParcelableExtra("anotherintent");
-
-        // redirect the embedded Intent
-        startActivity(forward); // Noncompliant
-    }
+protected void test (HttpServletRequest req, HttpServletResponse resp) throws IOException {
+  String location = req.getParameter("url");
+  resp.sendRedirect(location); // Noncompliant
 }
