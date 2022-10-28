d9eb8eb2383164d846e817a171380c6894dab913
lagccl <30540361+lagccl@users.noreply.github.com>
2022-10-28 15:43:30 -0300

    Update test.java

--- src/main/java/org/soyphea/k8s/srevice/test.java
+++ src/main/java/org/soyphea/k8s/srevice/test.java
@@ -1,17 +1,13 @@
-import javax.crypto.Cipher;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.NoSuchPaddingException;
+public class test extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // The intent used to start this exported component is retrieved
+        Intent intent = getIntent();
 
-public class test {
-   try {
-        Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
-        Cipher c7 = Cipher.getInstance("DESede"); // Noncompliant: Triple DES is vulnerable to meet-in-the-middle attack
-        Cipher c13 = Cipher.getInstance("RC2"); // Noncompliant: RC2 is vulnerable to a related-key attack
-        Cipher c19 = Cipher.getInstance("RC4"); // Noncompliant: vulnerable to several attacks (see https://en.wikipedia.org/wiki/RC4#Security)
-        Cipher c25 = Cipher.getInstance("Blowfish"); // Noncompliant: Blowfish use a 64-bit block size makes it vulnerable to birthday attacks     
-   }
-   catch(NoSuchAlgorithmException|NoSuchPaddingException e)
-   {      
-   }
-        
+        // extract the embedded Intent
+        Intent forward = (Intent) intent.getParcelableExtra("anotherintent");
+
+        // redirect the embedded Intent
+        startActivity(forward); // Noncompliant
+    }
 }
