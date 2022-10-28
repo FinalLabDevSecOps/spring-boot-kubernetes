b8b3d67e2fc6a0502673e5b38a0b9f6038caff3e
lagccl <30540361+lagccl@users.noreply.github.com>
2022-10-28 16:08:59 -0300

    Delete run.java

--- src/main/java/org/soyphea/k8s/srevice/run.java
+++
@@ -1,4 +0,0 @@
-public void run(javax.servlet.http.HttpServletRequest request) throws ClassNotFoundException {
-        String name = request.getParameter("name");
-        Class clazz = Class.forName(name);  // Noncompliant
-    }
